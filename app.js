1babdfda887e2d009654704f24af202aabd664a4
Francisco <fanikufran6@gmail.com>
2022-04-24 04:49:46 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const buttons = document.querySelectorAll('.ripple');
+const themeicon = document.querySelector('#themeicon');
+const theme = document.querySelector('link');
+
+const imgs = document.getElementById('imgs');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+const img = document.querySelectorAll('#imgs img');
+
+let idx = 0;
+
+let interval = setInterval(run, 2000);
+
+let now_playing = document.querySelector(".now-playing");
+let track_art = document.querySelector(".track-art");
+let track_name = document.querySelector(".track-name");
+let track_artist = document.querySelector(".track-artist");
+
+let playpause_btn = document.querySelector(".playpause-track");
+let next_btn = document.querySelector(".next-track");
+let prev_btn = document.querySelector(".prev-track");
+
+let seek_slider = document.querySelector(".seek_slider");
+let volume_slider = document.querySelector(".volume_slider");
+let curr_time = document.querySelector(".current-time");
+let total_duration = document.querySelector(".total-duration");
+
+let track_index = 0;
+let isPlaying = false;
+let updateTimer;
+
+themeicon.addEventListener('click', changeTheme)
+
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+        const x = e.clientX
+        const y = e.clientY
+
+        const buttonTop = e.target.offsetTop
+        const buttonLeft = e.target.offsetLeft
+
+        const xInside = x - buttonLeft
+        const yInside = y - buttonTop
+
+        const circle = document.createElement('span')
+        circle.classList.add('circle')
+        circle.style.top = yInside + 'px'
+        circle.style.left = xInside + 'px'
+
+        this.appendChild(circle)
+
+        setTimeout(() => circle.remove(), 500)
+    })
+})
+
+function changeTheme(){
+    if (theme.getAttribute('href') == 'light.css') {
+        theme.setAttribute('href', 'dark.css');
+        themeicon.setAttribute('class', 'fas fa-sun');
+    } else {
+        theme.setAttribute('href', 'light.css');
+        themeicon.setAttribute('class', 'fas fa-moon');
+    }
+}
+
+function run() {
+    idx++
+    changeImage()
+}
+
+function changeImage() {
+    if(idx > img.length - 1) {
+        idx = 0
+    } else if(idx < 0) {
+        idx = img.length - 1
+    }
+
+    imgs.style.transform = `translateX(${-idx * 500}px)`
+}
+
+function resetInterval() {
+    clearInterval(interval)
+    interval = setInterval(run, 2000)
+}
