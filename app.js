56b22e1123e41351dacd6f2e4959a32d4bdd8c06
fanifrancs <fanikufran6@gmail.com>
2022-04-26 01:21:52 +0100

    cleaned up

--- app.js
+++ app.js
@@ -1,58 +1,13 @@
-const buttons = document.querySelectorAll('.ripple');
 const themeicon = document.querySelector('#themeicon');
 const theme = document.querySelector('link');
-
 const imgs = document.getElementById('imgs');
-const leftBtn = document.getElementById('left');
-const rightBtn = document.getElementById('right');
-
 const img = document.querySelectorAll('#imgs img');
-
+const downloadBtn = document.querySelector('.download');
 let idx = 0;
-
 let interval = setInterval(run, 2000);
 
-let now_playing = document.querySelector(".now-playing");
-let track_art = document.querySelector(".track-art");
-let track_name = document.querySelector(".track-name");
-let track_artist = document.querySelector(".track-artist");
-
-let playpause_btn = document.querySelector(".playpause-track");
-let next_btn = document.querySelector(".next-track");
-let prev_btn = document.querySelector(".prev-track");
-
-let seek_slider = document.querySelector(".seek_slider");
-let volume_slider = document.querySelector(".volume_slider");
-let curr_time = document.querySelector(".current-time");
-let total_duration = document.querySelector(".total-duration");
-
-let track_index = 0;
-let isPlaying = false;
-let updateTimer;
-
-themeicon.addEventListener('click', changeTheme)
-
-buttons.forEach(button => {
-    button.addEventListener('click', function (e) {
-        const x = e.clientX
-        const y = e.clientY
-
-        const buttonTop = e.target.offsetTop
-        const buttonLeft = e.target.offsetLeft
-
-        const xInside = x - buttonLeft
-        const yInside = y - buttonTop
-
-        const circle = document.createElement('span')
-        circle.classList.add('circle')
-        circle.style.top = yInside + 'px'
-        circle.style.left = xInside + 'px'
-
-        this.appendChild(circle)
-
-        setTimeout(() => circle.remove(), 500)
-    })
-})
+themeicon.addEventListener('click', changeTheme);
+downloadBtn.addEventListener('click', getVivo);
 
 function changeTheme(){
     if (theme.getAttribute('href') == 'light.css') {
@@ -64,22 +19,25 @@ function changeTheme(){
     }
 }
 
+function getVivo(){
+    location.href = 'https://www.mediafire.com/file/694rv948po5cfoj/Vivo_%25282021%2529_uploaded_by_Francisco.mp4/file';
+}
+
 function run() {
-    idx++
-    changeImage()
+    idx++;
+    changeImage();
 }
 
 function changeImage() {
     if(idx > img.length - 1) {
-        idx = 0
+        idx = 0;
     } else if(idx < 0) {
-        idx = img.length - 1
+        idx = img.length - 1;
     }
-
-    imgs.style.transform = `translateX(${-idx * 500}px)`
+    imgs.style.transform = `translateX(${-idx * 500}px)`;
 }
 
 function resetInterval() {
-    clearInterval(interval)
-    interval = setInterval(run, 2000)
+    clearInterval(interval);
+    interval = setInterval(run, 2000);
 }
